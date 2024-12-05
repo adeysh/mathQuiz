14d35b52a55e80316716a01b422e4062c6285ab3
Adesh Katiya <adeshkatiya27@gmail.com>
2024-12-05 17:29:32 +0530

    getting the score from localStorage, generating questions with random operators, giving notifications with toastify, validating user input

--- index.js
+++ index.js
@@ -3,17 +3,64 @@ const answerInputEl = document.getElementById('answerInput');
 const scoreEl = document.getElementById("score");
 const submitBtn = document.getElementById('submitBtn');
 
+// get the score from localStorage
+document.addEventListener("DOMContentLoaded", (event) => {
+    if (localStorage.getItem("score") !== null) {
+        let score = localStorage.getItem("score");
+        scoreEl.innerText = `Score -> ${score}`;
+    } else {
+        scoreEl.innerText = `Score -> 0`;
+    }
+});
 
+// get random integer
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// get random operator
+function getRndOperator() {
+    const operators = {
+        1: "addition",
+        2: "subtraction",
+        3: "multiplication",
+        4: "division",
+    };
+
+    const randomInt = getRndInteger(1, 4);
+    const operator = operators[randomInt];
+
+    return operator;
+}
+
+// generate random question
 function generateQuestion() {
     const randomInt1 = getRndInteger(1, 10);
     const randomInt2 = getRndInteger(1, 10);
+    const randomOperator = getRndOperator();
 
-    const question = `What is ${randomInt1} multiply by ${randomInt2} ?`;
-    const answer = randomInt1 * randomInt2;
+    let question = "";
+    let answer = 0;
+    switch (randomOperator) {
+        case "addition":
+            question = `What is ${randomInt1} added to ${randomInt2} ?`;
+            answer = randomInt1 + randomInt2;
+            break;
+        case "subtraction":
+            question = `What is ${randomInt1} subtracted from ${randomInt2} ?`;
+            answer = randomInt2 - randomInt1;
+            break;
+        case "multiplication":
+            question = `What is ${randomInt1} multiply by ${randomInt2} ?`;
+            answer = randomInt1 * randomInt2;
+            break;
+        case "division":
+            question = `What is ${randomInt1} divided by ${randomInt2} ?`;
+            answer = (randomInt1 / randomInt2).toFixed(1);
+            break;
+        default:
+            break;
+    }
 
     return {
         question: question,
@@ -21,15 +68,15 @@ function generateQuestion() {
     };
 }
 
-
+// display question
 function displayQuestion(questionAnsObj) {
     questionEl.innerText = questionAnsObj.question;
 }
 
-
+// getting user input and checking calculation
 function getUserInputAndCheck(questionAnsObj) {
     let isCorrect = false;
-    if (questionAnsObj.answer === Number(answerInputEl.value)) {
+    if (Number(questionAnsObj.answer) === Number(answerInputEl.value)) {
         isCorrect = true;
         return isCorrect;
     } else {
@@ -37,22 +84,61 @@ function getUserInputAndCheck(questionAnsObj) {
     }
 }
 
+// updating the score
 let score = 0;
 function changeScore(isCorrectValue) {
     score += isCorrectValue ? 1 : -1;
+    localStorage.setItem("score", score);
+
+    // giving feedback with toastify library
+    if (isCorrectValue) {
+        Toastify({
+            text: `You are correct and your score is ${score}`,
+            duration: 2000,
+            gravity: "bottom", // Position: "top" or "bottom"
+            position: "center", // Align: "left", "center", or "right"
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+        }).showToast();
+    } else {
+        Toastify({
+            text: `Incorrect! Your score is now ${score}`,
+            duration: 2000,
+            gravity: "bottom",
+            position: "center",
+            style: {
+                background: "linear-gradient(to right, #f23a3a, #e3cc00)",
+            },
+        }).showToast();
+    }
 
     questionAnsObj = generateQuestion();
     displayQuestion(questionAnsObj);
-    answerInputEl.innerText = "";
     scoreEl.innerText = `Score -> ${score}`;
 }
 
 let questionAnsObj = generateQuestion();
+getRndOperator();
 displayQuestion(questionAnsObj);
 
 
 submitBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    const isCorrectValue = getUserInputAndCheck(questionAnsObj);
-    changeScore(isCorrectValue);
+    // checking valid user input 
+    let isValid = true;
+    const value = answerInputEl.value ? Number(answerInputEl.value) : "";
+    if (value < -100 || value > 100 || isNaN(value) || value == "") {
+        isValid = false;
+        answerInputEl.setCustomValidity("Enter a number between -100 and 100.");
+        answerInputEl.reportValidity();
+    } else {
+        answerInputEl.setCustomValidity("");
+    }
+
+    if (isValid) {
+        event.preventDefault();
+        const isCorrectValue = getUserInputAndCheck(questionAnsObj);
+        changeScore(isCorrectValue);
+        answerInputEl.value = "";
+    }
 });
